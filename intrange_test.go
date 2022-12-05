ea0a09f313d189745b91c165577634247790fd90
Ryan Punt <ryan@mirum.org>
2022-12-05 11:41:28 -0600

    equality if also overlap

--- intrange_test.go
+++ intrange_test.go
@@ -10,10 +10,10 @@ func TestContains(t *testing.T){
 	r2 := Intrange{3,7}
 	r3 := Intrange{2,4}
 	if got := r1.Contains(r2); got != false {
-		t.Errorf("Contains: r1 should not contain r2")
+		t.Errorf("Contains: r1 does not contain r2")
 	}
 	if got := r1.Contains(r3); got != true {
-		t.Errorf("Contains: r1 should contain r3")
+		t.Errorf("Contains: r1 does contain r3")
 	}
 }
 
@@ -24,13 +24,13 @@ func TestEquals(t *testing.T) {
 	r3 := Intrange{1,3}
 	r4 := Intrange{3,7}
 	if got := r1.Equals(r2); got != true {
-		t.Errorf("Contains: r1 should equal r2")
+		t.Errorf("Equals: r1 does equal r2")
 	}
 	if got := r1.Equals(r3); got != false {
-		t.Errorf("Contains: r1 should not equal r3")
+		t.Errorf("Equals: r1 does not equal r3")
 	}
 	if got := r1.Equals(r4); got != false {
-		t.Errorf("Contains: r1 should not equal r4")
+		t.Errorf("Equals: r1 does not equal r4")
 	}
 }
 
@@ -43,19 +43,24 @@ func TestOverlaps(t *testing.T) {
 	r5 := Intrange{-5,0}
 	r6 := Intrange{83,91}
 	r7 := Intrange{82,97}
+	r8 := Intrange{82,97}
 	if got := r1.Overlaps(r2); got != true {
-		t.Errorf("Contains: r1 should overlap r2")
+		t.Errorf("Overlaps: r1 does overlap r2")
 	}
 	if got := r1.Overlaps(r3); got != true {
-		t.Errorf("Contains: r1 should not overlap r3")
+		t.Errorf("Overlaps: r1 does not overlap r3")
 	}
 	if got := r1.Overlaps(r4); got != false {
-		t.Errorf("Contains: r1 should not overlap r4")
+		t.Errorf("Overlaps: r1 does not overlap r4")
 	}
 	if got := r1.Overlaps(r5); got != false {
-		t.Errorf("Contains: r1 should not overlap r5")
+		t.Errorf("Overlaps: r1 does not overlap r5")
 	}
 	if got := r6.Overlaps(r7); got != true {
-		t.Errorf("Contains: r6 should overlap r7")
+		t.Errorf("Overlaps: r6 does overlap r7")
+	}
+	// equality is technically also overlap
+	if got := r7.Overlaps(r8); got != true {
+		t.Errorf("Overlaps: r7 does overlap r8")
 	}
 }
